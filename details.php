b25983380cc587ebfe009735f561c329d41104b9
Trisha Garcia <garcia-patricia@live.com>
2021-10-16 15:59:38 -0400

    cleanup and fixed dynamic pages

--- details.php
+++ details.php
@@ -1,30 +1,30 @@
-<?php 
-if (isset($_GET['id'])) {
-require_once 'includes/connect.php';
 
-$sql = "SELECT * FROM tbl_project WHERE project_id='$id' ";
+<?php
+include_once 'sections/header.html';
+?>
+
+<?php 
+//query
+if (isset($_GET['project'])) {
+require_once 'includes/connect.php';
+$project = mysqli_real_escape_string($conn, $_GET['project']);
+
+$sql = "SELECT * FROM tbl_project WHERE project_id= '$project' ";
 $result = mysqli_query($conn, $sql) or die ("Bad query : $sql ");
 $row = mysqli_fetch_array($result);
 
 } else {
     header ('Location: index.php');
 }
-// run this query to echo the information on the page. the top query is for the id
-$proj = " SELECT * FROM tbl_project;";
-    $project_result = mysqli_query($conn, $proj);
-    // this id '$projects' is where the data is stored
-    $projects = mysqli_fetch_array($project_result);
 ?>
 
-<?php
-//query
-include_once 'includes/connect.php';
-include_once 'sections/header.html';
-?>
+
+
+
 
 <section class="proj-header-image">
     <p class="project-details-page-title">
-        <?php echo $projects['project_name']; ?> <br><br>
+        <?php echo $row['project_name']; ?> <br><br>
 </p>
 </section>
 
@@ -38,18 +38,18 @@ include_once 'sections/header.html';
                     <li>
                         <span class="bold details-list-small-title">role(s)</span>
                                 <span class="details-sm-divider"></span>
-                                        <?php echo $projects['project_roles']; ?> </li>
+                                        <?php echo $row['project_roles']; ?> </li>
                     <li>
                         <span class="bold details-list-small-title">project type</span>
                         <span class="details-sm-divider"></span>
-                        <?php echo $projects['project_type']; ?> </li>
+                        <?php echo $row['project_type']; ?> </li>
                     <li><span class="bold details-list-small-title">year</span>
-                    <span class="details-sm-divider"></span><?php echo $projects['project_year']; ?> </li>
+                    <span class="details-sm-divider"></span><?php echo $row['project_year']; ?> </li>
                 </ul>
             </div>
             <div class="overview-proj">
                 <h3 class="overview-title">overview</h3>
-                <?php echo $projects['project_objective']; ?> 
+                <?php echo $row['project_objective']; ?> 
             </div>
         </div>
     </div>
@@ -62,17 +62,17 @@ include_once 'sections/header.html';
 <section class="proj-details-result">
     <div class="wrapper">
         <p>
-            <?php echo $projects['project_result']; ?> 
+            <?php echo $row['project_result']; ?> 
         </p>
     </div> 
 </section>
 
 <div class="proj-view-container">
     <div class="project-views">
-            <img src="img/<?php echo $projects['project_view_1'];?>" alt="TBE">
-            <img src="img/<?php echo $projects['project_view_2'];?>" alt="TBE">
-            <img src="img/<?php echo $projects['project_view_3'];?>" alt="TBE">
-            <img src="img/<?php echo $projects['project_view_4'];?>" alt="TBE">
+            <img src="img/<?php echo $row['project_view_1'];?>" alt="TBE">
+            <img src="img/<?php echo $row['project_view_2'];?>" alt="TBE">
+            <img src="img/<?php echo $row['project_view_3'];?>" alt="TBE">
+            <img src="img/<?php echo $row['project_view_4'];?>" alt="TBE">
     </div>
 </div>
 
--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<? 
-$page = (empty($_GET["id"])) ? "home" : $_GET["id"];
-$page = basename($page).".php"; 
-if (is_readable($page)) { 
-  include($page); 
-} else {
-  header("HTTP/1.0 404 Not Found"); 
-  exit;  
-} 
-?>
+
 
 <?php
 //query
@@ -24,71 +15,9 @@ include_once 'sections/header.html';
 include_once 'sections/intro.html';
 ?>  
 
-
 <?php
-    $sql = " SELECT * FROM tbl_project;";
-    $result = mysqli_query($conn, $sql);
-    $projects = array();
-    
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $projects[] = $row;
-        }
-    }
+include_once 'sections/project-loop.php';
 ?>
-
-<section id="gallery-container">
-
-<?php foreach($projects as $project): ?>
-<div class="project-container" style="background-image: url(img/<?php echo $project['project_thumbnail'];?>)">
-    <a class="modal-button proj-title" href="#<?php echo $project['project_link'];?>">
-            <?php echo $project['project_name'];?><br>
-            
-    </a>
-
-</div>    
-
-<!-- modal -->
-<div id="<?php echo $project['project_link'];?>" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <div class="modal-header" style="background-image: url(img/<?php echo $project['project_image'];?>) ; background-repeat: no-repeat ; height:100%; background-position: center; background-size: cover;" ></div>
-          <div class="modal-body  ">
-                <h2 class="project-modal-title"><?php echo $project['project_name'];?></h2>
-                        <span class="divider"></span>
-             <p class="project-modal-text">Project over view.<?php echo $project['project_objective'];?>or re-watching sitcom classics. If you're interested in learning more about me and my design background you can click that big ol' button down there - enjoy!</p>
-            <div class="btn-container-modal">
-
-              <a href="details.php?id=<?php echo $project['project_id'];?>"class="btn-view-modal">
-                  <div class="main-btn">view</div>
-              </a>
-
-            </div>
-            <div class="modal-project-tools">
-              tools used
-            </div>
-            </div>
-           
-
-    </div>
-
-    </div>
-
-    
-  </div>
-
-</div>
-
-<?php endforeach; ?>
-
-
-</div>
-
-</section>
-
-
-
 
 <?php
 include_once 'sections/footer.php';
